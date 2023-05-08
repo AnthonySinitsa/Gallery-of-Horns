7343ca319e492517b552a20ff1fc8d9c9bd96764
AnthonySinitsa <itonyemail@gmail.com>
2023-05-08 23:03:58 +0000

    assignemnt done

--- src/App.js
+++ src/App.js
@@ -2,25 +2,17 @@
 import React from 'react';
 //import compenent files that i want to use in App
 import Main from './Main.js';
+import Header from './Header.js';
+import Footer from './Footer.js';
 
 //2. declare the class
 class App extends React.Component {
   render() {
     return (
       <>
-        <header>
-          <h1>Welcome to 301!</h1>
-        </header>
-        <main>
-          <article>
-            <h3>other title</h3>
-            <p>paragraph</p>
-          </article>
-        </main>
+        <Header />
         <Main />
-        <footer>
-          &copy;2023Sinitsa
-        </footer>
+        <Footer />
       </>
     );
   }
